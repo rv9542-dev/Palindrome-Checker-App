02d0f26b363d82debe2c18abd73e311d21c826b7
sb2165-hue <sb2165@srmist.edu.in>
2026-02-25 16:06:14 +0530

    [Ravi Chandra Reddy] Added [UC1: Application entry & welcome message]

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+/*
+author:V.Ravi Chandra Reddy
+version:1.0
+ */
+
+
+
+class PalindroneCheckerApp{
+    public static void main(String args[])
+    {
+        System.out.println("welcome to the palindrome checker management system");
+        System.out.println("version 1.0");
+        System.out.println("system initialied successfully");
+    }
+}
